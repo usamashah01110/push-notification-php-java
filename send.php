1b98946e0a7f7f5482a4be696c3c378c4f3b2c0d
Usama Bukhari <usamaqadeer.te@gmail.com>
2023-10-20 16:24:33 +0500

    added more code

--- send.php
+++ send.php
@@ -10,7 +10,7 @@ $server_key = "AAAALbwA93g:APA91bHcaaA3KwOJRn42EDPJqmWTvRRTKREVL8Om4VNG__A3wLJcD
   "image" => "https://images.unsplash.com/photo-1514473776127-61e2dcldded3?w=871&q=80",
    "click_action" => "http ://example.com"),
    "registration_ids" => [
-    "dp5kVKlho9s58spHJoy0uk:APA91bFCa4TZ1CZ9OyUuz1aYHJmzLuHdU9ntfz-UkU91zvYhbisYrJ35NMdjCEyOwUjBIc5scENCmSZGUJ5FYthcqQa95UhG1qGYx05uprbKdL0bT4xhmilycaLkFvlZVXZbO4dI5abt"
+    "dp5kVKlho9s58spHJoy0uk:APA91bHLZPpUwbTnCHvZv8Zk493tbyU2X0Q2eyfUnGS_6jVwrc7InAVBU17NtR7GTULLhG18467L7GPgBuxOadQ-6SqmP7XwBoJtXmWJ9v5mfht0w2QAsacTs4hSd9SZIdUlDrV49uX6"
    ]
    );
 
@@ -28,49 +28,3 @@ if($response === false){
     { echo "Message sent successfully"; } 
     
     curl_close($curl); 
-
-
-
-
-// include "./get_access_token.php";
-// function sendFCMNotification($access_token, $token) {
-//     $url = "https://fcm.googleapis.com/v1/projects/push-notification-acca2/messages:send";
-//     $data = [
-//         'message' => [
-//             "data"=> [
-//                 "title" => "Title",
-//                 "body" => "This is message body.",
-//                 "icon" => "https://www.clipscutter.com/image/brand/brand-256.png",
-//                 "image" => "https://images.unsplash.com/photo-1514473776127-61e2dc1dded3?w=871&q=80",
-//                 "click_action" => "https://example.com"
-//             ],
-//             'token' => $token
-//         ]
-//     ];
-//     $options = array(
-//         CURLOPT_URL => $url,
-//         CURLOPT_POST => true,
-//         CURLOPT_HTTPHEADER => array(
-//             "Authorization: Bearer " . $access_token,
-//             "Content-Type: application/json",
-//         ),
-//         CURLOPT_POSTFIELDS => json_encode($data),
-//     );
-//     $curl = curl_init();
-//     curl_setopt_array($curl, $options);
-//     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//     $response = curl_exec($curl);
-//     curl_close($curl);
-//     return $response;
-// }
-
-// $access_token = get_access_token("push-notification-acca2-firebase-adminsdk-ixqhr-631512016c.json");
-// $device_tokens = [
-//     "device-token-here",
-//     "device-token-here"
-// ];
-
-// foreach ($device_tokens as $token) {
-//     $response = sendFCMNotification($access_token, $token);
-//     echo $response . '<br>';
-// }
